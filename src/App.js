039dc4cfe26af465fd51aff6cfed45bce8432f12
able24 <egbeleabel@yahoo.com>
2023-02-17 15:43:06 +0000

    initial react base code

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Home from './pages/Home';
+import Contact from './pages/Contact';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import './App.css';
+
+function App() {
+  return (
+    <>
+    <Header />
+
+    <Footer />
+    </>
+
+  )
+
+}
+
+export default App;
