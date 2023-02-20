7b34340e10edc7c04148d2ada7a2c75087c0e005
innonka <innon@yandex.ru>
2023-02-20 20:29:52 +0200

    add style using tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,42 @@
-import {NavLink} from 'react-router-dom';
+import { NavLink } from "react-router-dom";
 
 function Header() {
-    return (
-      <header>
-        <h3>LOGO</h3>
+  return (
+    <header className="max-w-2xl mx-auto">
+      <nav className="text-right p-2 ">
+        <NavLink to="/">
+          <button
+            type="button"
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-white font-medium text-xs leading-tight uppercase rounded hover:text-blue-600 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 focus:text-black transition duration-150 ease-in-out"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            Home
+          </button>
+        </NavLink>
+        <NavLink to="/Contact">
+          <button
+            type="button"
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-white font-medium text-xs leading-tight uppercase rounded hover:text-blue-600 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 focus:text-black transition duration-150 ease-in-out"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            Contact
+          </button>
+        </NavLink>
+        <NavLink to="/About">
+          <button
+            type="button"
+            className="inline-block px-6 py-2.5 mr-2 bg-transparent text-white font-medium text-xs leading-tight uppercase rounded hover:text-blue-600 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 focus:text-black transition duration-150 ease-in-out"
+            data-mdb-ripple="true"
+            data-mdb-ripple-color="light"
+          >
+            About
+          </button>
+        </NavLink>
+      </nav>
+    </header>
+  );
+}
 
-        <nav>
-          <NavLink to='/'>Home</NavLink>
-          <NavLink to='/Contact'>Contact</NavLink>
-        </nav>
-        
-      </header>
-    );
-  }
-  
-  export default Header;
+export default Header;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -52,35 +52,55 @@ function Home() {
 
   return (
     /*Input area for user to type in and modal for AI response*/
-    <div>
-      <h1 className="text-xl font-bold mb-4">What Would You Like To Know?</h1>
-
-      <input
-        className="mb-2 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        value={userQuestion}
-        onChange={handleChange}
-        type="text"
-        placeholder="Type in your question"
-      ></input>
-      <button onClick={promptAI} className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Submit</button>
-
-      {modalOpen && showDiv && (
-        <div style={styles.modal}>
-          <p>
-            <strong>Answer:</strong> {modalData}
-          </p>
-          <button
-            
-            onClick={() => {
-              setShowDiv(false);
-              setUserQuestion("");
+    <>
+      <div className="container mx-auto  max-w-2xl">
+        <div className="text-center text-gray-800">
+          <div
+            className="mt-4 block rounded-lg shadow-lg px-6 py-12 md:py-16 md:px-12"
+            style={{
+              backgroundColor: "hsla(0, 0%, 100%, 0.8)",
+              backdropFilter: "saturate(200%) blur(25px)",
             }}
           >
-            Close
-          </button>
+            <h1 className="text-2xl sm:text-5xl md:text-6xl xl:text-7xl font-bold tracking-tight mb-12">
+              Study Buddy <br />
+              <span className="text-blue-600">Your AI Assistant</span>
+            </h1>
+
+            <input
+              className="mb-2 outline-none focus:ring-offset-2 focus:ring-2 w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              value={userQuestion}
+              onChange={handleChange}
+              type="text"
+              placeholder="Type in your question"
+            ></input>
+
+            <button
+              onClick={promptAI}
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+            >
+              Submit
+            </button>
+          </div>
         </div>
-      )}
-    </div>
+
+        {modalOpen && showDiv && (
+          <div style={styles.modal}>
+            <p>
+              <strong>Answer:</strong> {modalData}
+            </p>
+            <button
+              onClick={() => {
+                setShowDiv(false);
+                setUserQuestion("");
+              }}
+            >
+              Close
+            </button>
+          </div>
+        )}
+      </div>
+    </>
   );
 }
 
